1ccd34445ea5e23987ffab71ac0ca4194f69be1c
_RC_ <randy.white.1986@gmail.com>
2023-09-20 12:20:40 -0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -22,8 +22,6 @@ dnf install -y ansible-core ansible ansible-collection-ansible-utils ansible-col
 #use this to install packages via ansible
 ansible-playbook --connection=local --inventory 127.0.0.1 ./site.yml
 
-
-
 #change directory to tmp place 
 pushd ${MAIN_INSTALL_TMP_DIR}
 
@@ -31,7 +29,7 @@ pushd ${MAIN_INSTALL_TMP_DIR}
 wget -O slurm-23.02-latest.tar.bz2 https://download.schedmd.com/slurm/slurm-23.02-latest.tar.bz2
 
 #grab latest munge
-wget -O munge-0.5.15.tar.gz https://github.com/dun/munge/tarball/master
+wget -O munge-0.5.15.tar.xz https://github.com/dun/munge/releases/download/munge-0.5.15/munge-0.5.15.tar.xz
 
 #grab apache directory studio
 wget -O apacheds-2.0.0.AM26.tar.gz https://dlcdn.apache.org//directory/apacheds/dist/2.0.0.AM26/apacheds-2.0.0.AM26.tar.gz
@@ -40,26 +38,25 @@ wget -O apacheds-2.0.0.AM26.tar.gz https://dlcdn.apache.org//directory/apacheds/
 wget -O prometheus-2.47.0.linux-amd64.tar.gz https://github.com/prometheus/prometheus/releases/download/v2.47.0/prometheus-2.47.0.linux-amd64.tar.gz
 
 # untars the archives 
-tar -axvf munge-0.5.15.tar.gz
+tar -axvf munge-0.5.15.tar.xz
 tar -axvf slurm-23.02-latest.tar.bz2
 tar -axvf apacheds-2.0.0.AM26.tar.gz
 tar -axvf prometheus-2.47.0.linux-amd64.tar.gz
 
 
 #start the building munge 
-pushd dun-munge*
-bash ./bootstrap
-
-bash configure --prefix=${MAIN_INSTALL_PREFIX}/munge --sysconfdir=/etc --localstatedir=/var --runstatedir=/run
-
+pushd munge-0.5.15
+bash ./configure --prefix=${MAIN_INSTALL_PREFIX}/munge --sysconfdir=/etc --localstatedir=/var --runstatedir=/run
+make -j4 && make install
+make clean
 popd 
 
 
 #install slurm
 pushd slurm-23.02.5/
-
-bash configure --prefix=${MAIN_INSTALL_PREFIX}/slurm --sysconfdir=/etc --localstatedir=/var --runstatedir=/run
-
+bash ./configure --prefix=${MAIN_INSTALL_PREFIX}/slurm --sysconfdir=/etc --localstatedir=/var --runstatedir=/run
+make -j4 && make install
+make clean
 popd 
 
 
@@ -67,11 +64,20 @@ popd
 #note that this is a java thing so not a lot to do here 
 mv apacheds-2.0.0.AM26 ${MAIN_INSTALL_PREFIX}/apacheds
 
+## prometheus install 
+mv prometheus-2.47.0.linux-amd64 ${MAIN_INSTALL_PREFIX}/prometheus
 
-#setup the environment
+#Create the profile for the environment 
+cat << EOF > /etc/profile.d/z99_hotep.sh
 
-export PATH=${HOME}/spindle_build/bin:${PATH}
-export LD_LIBRARY_PATH=${HOME}/spindle_build/lib:${LD_LIBRARY_PATH}
-export LD_LIBRARY_PATH=${HOME}/spindle_build/lib64:${LD_LIBRARY_PATH}
-export MANPATH=${HOME}/spindle_build/share/man:${MANPATH}
-export CPATH=${HOME}/spindle_build/include:${CPATH}
+PATH=${MAIN_INSTALL_PREFIX}/munge/bin:${MAIN_INSTALL_PREFIX}/slurm/bin:\${PATH}
+LD_LIBRARY_PATH=${MAIN_INSTALL_PREFIX}/munge/lib:${MAIN_INSTALL_PREFIX}/slurm/lib:${MAIN_INSTALL_PREFIX}/munge/lib64:${MAIN_INSTALL_PREFIX}/slurm/lib64:\${LD_LIBRARY_PATH}
+CPATH=${MAIN_INSTALL_PREFIX}/munge/include:${MAIN_INSTALL_PREFIX}/slurm/include:\${CPATH}
+
+export PATH
+export LD_LIBRARY_PATH
+export CPATH
+
+EOF
+#add manpath to profile
+#export MANPATH=${HOME}/spindle_build/share/man:${MANPATH}
